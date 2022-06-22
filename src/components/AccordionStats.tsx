c376f6185636f7e876b41ae612cb1a202b85993c
Michal Sokolik <msokolik55@gmail.com>
2022-06-22 14:51:19 +0200

    fix: sorting answered questions

--- src/components/AccordionStats.tsx
+++ src/components/AccordionStats.tsx
@@ -23,6 +23,8 @@ const AccordionStats = ({
 	generateQuestion,
 	keyLabel,
 }: IPropsStats) => {
+	const sortedItems = [...items].sort((a, b) => a - b);
+
 	return (
 		<Accordion style={{ width: "100%" }}>
 			<AccordionSummary expandIcon={<ExpandMore />}>
@@ -34,13 +36,14 @@ const AccordionStats = ({
 				</Grid>
 			</AccordionSummary>
 			<AccordionDetails style={{ flexWrap: "wrap" }}>
-				{items
-					.sort((a, b) => a - b)
-					.map((num) => (
-						<Button key={keyLabel + num} onClick={() => generateQuestion(num)}>
-							{num + 1}
-						</Button>
-					))}
+				{sortedItems.map((num) => (
+					<Button
+						key={keyLabel + num}
+						onClick={() => generateQuestion(num)}
+					>
+						{num + 1}
+					</Button>
+				))}
 			</AccordionDetails>
 		</Accordion>
 	);
