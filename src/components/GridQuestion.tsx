77d3166a57197fec5f64d3e3393ef316d16bfe05
Michal Sokolik <msokolik55@gmail.com>
2022-07-10 20:48:40 +0200

    feat: added component Option

--- src/components/GridQuestion.tsx
+++ src/components/GridQuestion.tsx
@@ -1,51 +1,21 @@
-import { SetStateAction } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 
 import { Button, Grid } from "@material-ui/core";
 import { ArrowBack, ArrowForward } from "@material-ui/icons";
 
-import {
-	answeredQuestionsAtom,
-	answeredWrongAtom,
-	hasAnsweredAtom,
-	questionIdAtom,
-} from "../state/atom";
+import { questionIdAtom } from "../state/atom";
 import { datasetLengthSelector, questionSelector } from "../state/selector";
+import Option from "./Option";
 
 interface IGridQuestion {
 	generateQuestion: (id?: number | undefined) => void;
 }
 
 const GridQuestion = (props: IGridQuestion) => {
-	const [answeredWrong, setAnsweredWrong] = useRecoilState(answeredWrongAtom);
-	const [answeredQuestions, setAnsweredQuestions] = useRecoilState(
-		answeredQuestionsAtom
-	);
 	const questionsCount = useRecoilValue(datasetLengthSelector);
 
 	const questionId = useRecoilValue(questionIdAtom);
 	const question = useRecoilValue(questionSelector);
-
-	const [hasAnswered, setHasAnswered] = useRecoilState(hasAnsweredAtom);
-
-	const insertId = (
-		items: number[],
-		setter: (value: SetStateAction<number[]>) => void,
-		id: number
-	) => {
-		if (!items.includes(id)) {
-			setter([...items, id]);
-		}
-	};
-
-	const checkAnswer = (id: number) => {
-		if (!question?.options[id].isRight)
-			insertId(answeredWrong, setAnsweredWrong, questionId);
-
-		insertId(answeredQuestions, setAnsweredQuestions, questionId);
-
-		setHasAnswered(true);
-	};
 
 	return (
 		<Grid item sm={7} style={{ width: "100%" }}>
@@ -53,25 +23,8 @@ const GridQuestion = (props: IGridQuestion) => {
 				{questionId + 1} / {questionsCount}
 			</h4>
 			<h4>{question?.title}</h4>
-			{question?.options.map((opt, idx) => (
-				<Button
-					key={idx}
-					style={{
-						color: "black",
-						backgroundColor: !hasAnswered
-							? "#EFEFEF"
-							: opt.isRight
-							? "green"
-							: "red",
-						width: "100%",
-						margin: "0.5rem 0 0.5rem 0",
-						textTransform: "none",
-					}}
-					disabled={hasAnswered}
-					onClick={() => checkAnswer(idx)}
-				>
-					{opt.title}
-				</Button>
+			{question?.options.map((option, idx) => (
+				<Option key={idx} option={option} index={idx} />
 			))}
 
 			<Grid container justifyContent="space-between">
---
+++ src/components/Option.tsx
@@ -0,0 +1,70 @@
+import { SetStateAction } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+
+import { Button } from "@material-ui/core";
+
+import { IOption } from "../models/Option";
+import {
+	answeredQuestionsAtom,
+	answeredWrongAtom,
+	hasAnsweredAtom,
+	questionIdAtom,
+} from "../state/atom";
+import { questionSelector } from "../state/selector";
+
+interface IOptionProps {
+	option: IOption;
+	index: number;
+}
+
+const Option = (props: IOptionProps) => {
+	const [answeredWrong, setAnsweredWrong] = useRecoilState(answeredWrongAtom);
+	const [answeredQuestions, setAnsweredQuestions] = useRecoilState(
+		answeredQuestionsAtom
+	);
+	const [hasAnswered, setHasAnswered] = useRecoilState(hasAnsweredAtom);
+
+	const questionId = useRecoilValue(questionIdAtom);
+	const question = useRecoilValue(questionSelector);
+
+	const insertId = (
+		items: number[],
+		setter: (value: SetStateAction<number[]>) => void,
+		id: number
+	) => {
+		if (!items.includes(id)) {
+			setter([...items, id]);
+		}
+	};
+
+	const checkAnswer = (id: number) => {
+		if (!question?.options[id].isRight)
+			insertId(answeredWrong, setAnsweredWrong, questionId);
+
+		insertId(answeredQuestions, setAnsweredQuestions, questionId);
+
+		setHasAnswered(true);
+	};
+
+	return (
+		<Button
+			style={{
+				color: "black",
+				backgroundColor: !hasAnswered
+					? "#EFEFEF"
+					: props.option.isRight
+					? "green"
+					: "red",
+				width: "100%",
+				margin: "0.5rem 0 0.5rem 0",
+				textTransform: "none",
+			}}
+			disabled={hasAnswered}
+			onClick={() => checkAnswer(props.index)}
+		>
+			{props.option.title}
+		</Button>
+	);
+};
+
+export default Option;
