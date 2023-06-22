67889a6b76cf3cf7d8e5c9ed79f1d6955c5b0836
Michal Sokolík <xsokolik@fi.muni.cz>
2023-06-21 22:16:22 -0400

    fix: changed datasets

--- netlify/functions/getDatasets.ts
+++ netlify/functions/getDatasets.ts
@@ -4,13 +4,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const handler: Handler = async (_event, _context) => {
-	// load all players from the database
-	const datasets = await prisma.dataset.findMany();
+	// const datasets = await prisma.dataset.findMany();
+	const datasets = ["hello", "world"];
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify(datasets, (_key, value) =>
-			// need to add a custom serializer because CockroachDB IDs map to
-			// JavaScript BigInts, which JSON.stringify has trouble serializing.
 			typeof value === "bigint" ? value.toString() : value
 		),
 	};
