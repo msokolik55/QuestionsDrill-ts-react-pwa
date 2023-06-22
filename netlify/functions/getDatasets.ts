b090be2b59d65c3b18fd13fedeb0e1231fc124c7
Michal Sokolík <xsokolik@fi.muni.cz>
2023-06-21 23:41:35 -0400

    fix: rollbacked comments

--- netlify/functions/getDatasets.ts
+++ netlify/functions/getDatasets.ts
@@ -1,13 +1,11 @@
 import { Handler } from "@netlify/functions";
 import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
-console.log("Hallo hallo");
-setTimeout(() => console.log("spim"), 10000);
+const prisma = new PrismaClient();
 
 const handler: Handler = async (_event, _context) => {
-	// const datasets = await prisma.dataset.findMany();
-	const datasets = ["hello", "world"];
+	const datasets = await prisma.dataset.findMany();
+	// const datasets = ["hello", "world"];
 
 	return {
 		statusCode: 200,
