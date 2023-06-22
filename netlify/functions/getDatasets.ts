6cc5c63b02cf360ed8767654eb6a1efca91fe098
Michal Sokolík <xsokolik@fi.muni.cz>
2023-06-21 22:24:33 -0400

    fix: uncommented

--- netlify/functions/getDatasets.ts
+++ netlify/functions/getDatasets.ts
@@ -1,5 +1,5 @@
 import { Handler } from "@netlify/functions";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // const prisma = new PrismaClient();
 
