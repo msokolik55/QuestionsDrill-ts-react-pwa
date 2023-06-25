e0315c57c2269f6255ab06115dc77c688a5b1873
Michal Sokolik <msokolik55@gmail.com>
2023-06-25 05:23:22 +0200

    feat: added missing name in dataset adding

--- netlify/functions/addDataset.ts
+++ netlify/functions/addDataset.ts
@@ -2,9 +2,16 @@ import { Handler } from "@netlify/functions";
 import prisma from "../../prisma/client";
 
 const handler: Handler = async (_event, _context) => {
+	const name = _event.body;
+	if (!name)
+		return {
+			statusCode: 400,
+			body: "Missing name for dataset.",
+		};
+
 	const dataset = await prisma.dataset.create({
 		data: {
-			name: "Test dataset",
+			name: name,
 		},
 	});
 
