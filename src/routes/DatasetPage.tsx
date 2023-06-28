69aa6dc0acbc0249db72e02f311c6897b07218bc
Michal Sokolik <msokolik55@gmail.com>
2023-06-28 06:14:11 +0200

    feat: added parsing URL dataset id

--- src/routes/DatasetPage.tsx
+++ src/routes/DatasetPage.tsx
@@ -1,8 +1,25 @@
 import { Container, Grid } from "@mui/material";
 import GridQuestion from "../components/question/GridQuestion";
 import GridStats from "../components/stats/GridStats";
+import { useLoaderData } from "react-router-dom";
+import { IDataset } from "../models/Dataset";
+
+interface ILoader {
+	params: {
+		datasetId: string;
+	};
+}
+
+export const loader = async ({ params }: ILoader) => {
+	const dataset = await fetch("/.netlify/functions/getDataset", {
+		body: params.datasetId,
+	});
+	return { dataset };
+};
 
 const DatasetPage = () => {
+	const dataset = useLoaderData();
+
 	return (
 		<Container>
 			<Grid container direction="row" spacing={4}>
