9fa1c60beaa4b53e9e71dc46a76ba7b62a7af885
Michal Sokolik <msokolik55@gmail.com>
2023-07-02 19:09:48 +0200

    feat: generating questions

--- src/routes/FormPage.tsx
+++ src/routes/FormPage.tsx
@@ -1,26 +1,69 @@
 import { Button } from "@mui/material";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Form } from "react-router-dom";
 
 export const action = async () => {
-	const dataset = await fetch("/.netlify/functions/addDataset", {
-		method: "post",
-		body: "test",
-	});
+	const dataset = null;
+	// const dataset = await fetch("/.netlify/functions/addDataset", {
+	// 	method: "post",
+	// 	body: "test",
+	// });
 	return { dataset };
+};
+
+interface IPropsQuestion {
+	idx: number;
+	setQuestions: Dispatch<SetStateAction<number[]>>;
+}
+
+const Question = (props: IPropsQuestion) => {
+	return (
+		<>
+			<Button
+				onClick={() => {
+					props.setQuestions((curr) =>
+						curr.filter((item) => item !== props.idx)
+					);
+				}}
+			>
+				X
+			</Button>
+			<label>Question name:</label>
+			<input name={`qst-${props.idx}`} />
+			<br />
+		</>
+	);
 };
 
 // TODO: design
 const FormPage = () => {
-	const [name, setName] = useState("");
+	const [questions, setQuestions] = useState<number[]>([]);
 
 	return (
 		<Form method="post">
-			<input
-				name="name"
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-			/>
+			<label>Dataset name:</label>
+			<input name="name" />
+
+			<Button
+				onClick={() =>
+					setQuestions((old) => [
+						...old,
+						questions.length > 0
+							? questions[questions.length - 1] + 1
+							: 0,
+					])
+				}
+			>
+				Add question
+			</Button>
+			<br />
+			{questions.map((val) => (
+				<Question
+					key={`qst-${val}`}
+					idx={val}
+					setQuestions={setQuestions}
+				/>
+			))}
 			<Button type="submit">Add dataset</Button>
 		</Form>
 	);
