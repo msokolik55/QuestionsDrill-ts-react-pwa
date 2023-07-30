b4e677a1678b43f3d4e3aa0d549fb7ba8bd096d5
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 07:05:08 +0200

    info: print log og response

--- src/appLoader.ts
+++ src/appLoader.ts
@@ -1,5 +1,6 @@
 export const loader = async () => {
 	const datasets = await fetch("/.netlify/functions/getDatasets");
+	console.log(datasets.body);
 	return datasets;
 	// return [{ id: "uuid01", name: "Dataset 1" }];
 };
