026948f5a5f067f1c0ebe5fffac5e0739831d306
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 06:59:42 +0200

    fix: finalized appLoader

--- src/appLoader.ts
+++ src/appLoader.ts
@@ -1,5 +1,5 @@
 export const loader = async () => {
-	// const datasets = await fetch("/.netlify/functions/getDatasets");
-	// return datasets;
-	return [{ id: "uuid01", name: "Dataset 1" }];
+	const datasets = await fetch("/.netlify/functions/getDatasets");
+	return datasets;
+	// return [{ id: "uuid01", name: "Dataset 1" }];
 };
