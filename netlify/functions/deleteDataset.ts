9eb957ed75b7a9ef5faaf10b9da8edd20432fc6d
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 08:25:24 +0200

    fix: where clause in deleteQuestions

--- netlify/functions/deleteDataset.ts
+++ netlify/functions/deleteDataset.ts
@@ -2,16 +2,16 @@ import { Handler } from "@netlify/functions";
 import prisma from "../../prisma/client";
 
 const handler: Handler = async (_event, _context) => {
-	const datasetId = JSON.parse(_event.body || "");
+	const dataset = JSON.parse(_event.body || "");
 
 	const deleteQuestions = prisma.question.deleteMany({
 		where: {
-			datasetId,
+			datasetId: dataset["id"],
 		},
 	});
 
 	const deleteDataset = prisma.dataset.delete({
-		where: { id: datasetId["id"] },
+		where: { id: dataset["id"] },
 	});
 
 	const transaction = await prisma.$transaction([
