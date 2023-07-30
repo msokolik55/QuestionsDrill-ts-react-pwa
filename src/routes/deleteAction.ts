43a6c0c8f401035f94d358672f25f437158240d6
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 07:44:04 +0200

    fix: uncommented api deleting

--- src/routes/deleteAction.ts
+++ src/routes/deleteAction.ts
@@ -4,10 +4,9 @@ export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
 	const data = Object.fromEntries(formData) as { [k: string]: string };
 
-	console.log(data);
-	// const dataset = await fetch("/.netlify/functions/removeDataset", {
-	// 	method: "delete",
-	// 	body: data["id"],
-	// });
+	await fetch("/.netlify/functions/removeDataset", {
+		method: "delete",
+		body: data["id"],
+	});
 	return data;
 };
