35db82de0d7dc88f3b11c517e6c4d59715b0d720
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 08:00:29 +0200

    fix: printing response

--- netlify/functions/deleteDataset.ts
+++ netlify/functions/deleteDataset.ts
@@ -3,6 +3,7 @@ import prisma from "../../prisma/client";
 
 const handler: Handler = async (_event, _context) => {
 	const datasetId = JSON.parse(_event.body || "");
+	console.log(datasetId);
 	const dataset = await prisma.dataset.delete({ where: { id: datasetId } });
 
 	return {
