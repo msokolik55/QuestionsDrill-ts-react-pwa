ed4472f11e2d9859516507b963d84ce65bee0b45
Michal Sokolik <msokolik55@gmail.com>
2023-07-30 08:22:24 +0200

    fix: prisma one by one deleting

--- netlify/functions/deleteDataset.ts
+++ netlify/functions/deleteDataset.ts
@@ -3,14 +3,25 @@ import prisma from "../../prisma/client";
 
 const handler: Handler = async (_event, _context) => {
 	const datasetId = JSON.parse(_event.body || "");
-	console.log(datasetId["id"]);
-	const dataset = await prisma.dataset.delete({
+
+	const deleteQuestions = prisma.question.deleteMany({
+		where: {
+			datasetId,
+		},
+	});
+
+	const deleteDataset = prisma.dataset.delete({
 		where: { id: datasetId["id"] },
 	});
 
+	const transaction = await prisma.$transaction([
+		deleteQuestions,
+		deleteDataset,
+	]);
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify(dataset, (_key, value) =>
+		body: JSON.stringify(transaction, (_key, value) =>
 			typeof value === "bigint" ? value.toString() : value
 		),
 	};
