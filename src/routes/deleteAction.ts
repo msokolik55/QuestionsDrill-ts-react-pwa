a379f98147a602eed76a69ca9a30e0eb867e8853
Michal Sokolik <msokolik55@gmail.com>
2023-08-07 03:50:13 +0200

    fix: typo in url deleteDataset

--- src/routes/deleteAction.ts
+++ src/routes/deleteAction.ts
@@ -3,11 +3,10 @@ import { ActionFunction } from "react-router";
 export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
 
-	const res = await fetch("/.netlify/functions/removeDataset", {
+	const res = await fetch("/.netlify/functions/deleteDataset", {
 		method: "post",
 		body: JSON.stringify(formData),
 	});
 
-	console.log(res);
 	return res;
 };
