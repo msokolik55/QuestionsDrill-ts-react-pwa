8b8cd1a6d661c10fc60858ef128d129f5753ecfa
Michal Sokolik <msokolik55@gmail.com>
2023-08-07 02:57:17 +0200

    fix: posting form data directly

--- src/routes/deleteAction.ts
+++ src/routes/deleteAction.ts
@@ -2,11 +2,11 @@ import { ActionFunction } from "react-router";
 
 export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
-	const data = Object.fromEntries(formData) as { [k: string]: string };
+	// const data = Object.fromEntries(formData) as { [k: string]: string };
 
 	const res = await fetch("/.netlify/functions/removeDataset", {
 		method: "post",
-		body: JSON.stringify(data),
+		body: JSON.stringify(formData),
 	});
 	return res;
 };
