8a58d09daad795b30a7207552b5f56dcbd4fa374
Michal Sokolik <msokolik55@gmail.com>
2023-08-07 03:46:30 +0200

    debug: log delete action

--- src/routes/deleteAction.ts
+++ src/routes/deleteAction.ts
@@ -3,11 +3,11 @@ import { ActionFunction } from "react-router";
 export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
 
-	console.log(Object.fromEntries(formData) as { [k: string]: string });
-
 	const res = await fetch("/.netlify/functions/removeDataset", {
 		method: "post",
 		body: JSON.stringify(formData),
 	});
+
+	console.log(res);
 	return res;
 };
