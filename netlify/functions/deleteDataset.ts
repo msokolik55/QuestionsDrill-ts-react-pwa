e20d9d12578cd31dcd70bfb8812cc25343b508c3
Michal Sokolik <msokolik55@gmail.com>
2023-08-07 03:19:12 +0200

    feat: cascade deleting

--- netlify/functions/deleteDataset.ts
+++ netlify/functions/deleteDataset.ts
@@ -4,24 +4,24 @@ import prisma from "../../prisma/client";
 const handler: Handler = async (_event, _context) => {
 	const dataset = JSON.parse(_event.body || "");
 
-	const deleteQuestions = prisma.question.deleteMany({
-		where: {
-			datasetId: dataset["id"],
-		},
-	});
+	// const deleteQuestions = prisma.question.deleteMany({
+	// 	where: {
+	// 		datasetId: dataset["id"],
+	// 	},
+	// });
 
-	const deleteDataset = prisma.dataset.delete({
+	const deleteDataset = await prisma.dataset.delete({
 		where: { id: dataset["id"] },
 	});
 
-	const transaction = await prisma.$transaction([
-		deleteQuestions,
-		deleteDataset,
-	]);
+	// const transaction = await prisma.$transaction([
+	// 	deleteQuestions,
+	// 	deleteDataset,
+	// ]);
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify(transaction, (_key, value) =>
+		body: JSON.stringify(deleteDataset, (_key, value) =>
 			typeof value === "bigint" ? value.toString() : value
 		),
 	};
